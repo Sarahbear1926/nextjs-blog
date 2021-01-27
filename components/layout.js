f9bfe002bfaf02d5a0e1552b9224131f86059cb7
Sarah Jung <srjung@ucsd.edu>
2021-01-27 10:24:14 -0800

    fixing img issue

--- components/layout.js
+++ components/layout.js
@@ -28,7 +28,7 @@ export default function Layout({ children, home }) {
         {home ? (
           <>
             <img
-              src=(prefix + "/images/profile.jpg")
+              src= "/images/profile.jpg"
               className={`${styles.headerHomeImage} ${utilStyles.borderCircle}`}
               alt={name}
             />
